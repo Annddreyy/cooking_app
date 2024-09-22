5f11c913c229cfbba17e5f38923c56b36911eda3
Annddreyy <andreymail22112006@gmail.com>
2024-09-22 19:06:33 +0300

    Add callories input field

--- app/src/main/java/com/example/cookingapp/MainActivity.java
+++ app/src/main/java/com/example/cookingapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.cookingapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recipe_page);
+        setContentView(R.layout.new_recipe);
+
+        Spinner spinner = findViewById(R.id.callories_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.callories_array,
+                R.layout.color_spinner_layout
+        );
+
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_layout);
+        spinner.setAdapter(adapter);
     }
 }
