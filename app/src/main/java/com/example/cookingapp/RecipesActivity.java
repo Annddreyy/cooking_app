87f3f3d7e9a121c3035cf6cfc3b42fa5c8b9326c
Annddreyy <andreymail22112006@gmail.com>
2024-10-06 12:21:57 +0300

    Add recipes from API

--- app/src/main/java/com/example/cookingapp/RecipesActivity.java
+++ app/src/main/java/com/example/cookingapp/RecipesActivity.java
@@ -1,10 +1,28 @@
 package com.example.cookingapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class RecipesActivity extends AppCompatActivity {
 
@@ -28,19 +46,66 @@ public class RecipesActivity extends AppCompatActivity {
             Intent intent = new Intent(view.getContext(), FavouritesActivity.class);
             view.getContext().startActivity(intent);});
 
-        LinearLayout recipeCardLayout2Button = findViewById(R.id.recipe_card_layout2);
-        recipeCardLayout2Button.setOnClickListener(view -> {
-            Intent intent = new Intent(view.getContext(), RecipePageActivity.class);
-            view.getContext().startActivity(intent);});
 
-        LinearLayout recipeCardLayout3Button = findViewById(R.id.recipe_card_layout3);
-        recipeCardLayout3Button.setOnClickListener(view -> {
-            Intent intent = new Intent(view.getContext(), RecipePageActivity.class);
-            view.getContext().startActivity(intent);});
+        new GetRecipesTask().execute("https://cooking-app-api.vercel.app/api/v1/recipes");
+    }
 
-        LinearLayout recipeCardLayout4Button = findViewById(R.id.recipe_card_layout4);
-        recipeCardLayout4Button.setOnClickListener(view -> {
-            Intent intent = new Intent(view.getContext(), RecipePageActivity.class);
-            view.getContext().startActivity(intent);});
+    private class GetRecipesTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                InputStream responseStream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(responseStream));
+
+                StringBuilder result = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+                return result.toString();
+            } catch (IOException e) {
+                return "Error: " + e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONArray jsonArray = new JSONArray(result);
+                LinearLayout recipes = findViewById(R.id.recipes_cards);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    String title = jsonObject.getString("title");
+                    String callories = jsonObject.getString("callories");
+                    String cookingTime = jsonObject.getString("cooking_time");
+
+                    recipes.addView(createRecipeCard(title, callories, cookingTime));
+                }
+            } catch (JSONException e) {
+                System.out.println("Error parsing JSON: " + e.getMessage());
+            }
+        }
+    }
+
+    protected View createRecipeCard(String title, String callories, String time) {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View card = inflater.inflate(R.layout.recipe_card, null);
+
+        TextView cardTitle = card.findViewById(R.id.recipe_card_title_text);
+        cardTitle.setText(title);
+
+        TextView calloriesText = card.findViewById(R.id.callories_card_text);
+        calloriesText.setText(callories + " калл.");
+
+        TextView timeText = card.findViewById(R.id.time_card_text);
+        timeText.setText(time + " мин.");
+
+        return card;
     }
 }
