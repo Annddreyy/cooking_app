004090e44624fa1376ed8bc97af95c5d7fa022f6
Annddreyy <andreymail22112006@gmail.com>
2024-10-15 23:42:54 +0300

    Fix screens

--- app/src/main/java/com/example/cookingapp/RegistrationActivity1.java
+++ app/src/main/java/com/example/cookingapp/RegistrationActivity1.java
@@ -1,6 +1,7 @@
 package com.example.cookingapp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -8,11 +9,24 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class RegistrationActivity1 extends AppCompatActivity {
+    ArrayList<AuthorizationInfo> authorizationInformation = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,8 @@ public class RegistrationActivity1 extends AppCompatActivity {
         EditText password = findViewById(R.id.password_input);
 
         Button registrationButton = findViewById(R.id.registration_button);
+
+        new GetAuthorizationTask().execute("https://cooking-app-api-andrey2211.amvera.io/api/v1/authorization");
 
         registrationButton.setOnClickListener(view -> {
             TextView errorText = findViewById(R.id.error_text);
@@ -39,10 +55,22 @@ public class RegistrationActivity1 extends AppCompatActivity {
                 boolean isCorrectEmail = matcher.find();
 
                 if (passwordText.length() >= 8 && isCorrectEmail) {
-                    Intent intent = new Intent(view.getContext(), RegistrationActivity2.class);
-                    intent.putExtra("email", emailText);
-                    intent.putExtra("password", SHA256.calculateSHA256(passwordText));
-                    view.getContext().startActivity(intent);
+
+                    boolean isNewEmail = true;
+
+                    for (AuthorizationInfo authorizationInfo: authorizationInformation) {
+                        if (Objects.equals(authorizationInfo.email, emailText)) {
+                            errorText.setText("Пользователь с такой почтой уде существует!");
+                            isNewEmail = false;
+                            break;
+                        }
+                    }
+                    if (isNewEmail) {
+                        Intent intent = new Intent(view.getContext(), RegistrationActivity2.class);
+                        intent.putExtra("email", emailText);
+                        intent.putExtra("password", SHA256.calculateSHA256(passwordText));
+                        view.getContext().startActivity(intent);
+                    }
                 }
                 else if (!isCorrectEmail)
                     errorText.setText("Почта введена в неверном формате!");
@@ -57,5 +85,48 @@ public class RegistrationActivity1 extends AppCompatActivity {
         autorizationLink.setOnClickListener(view -> {
             Intent intent = new Intent(view.getContext(), AutorizationActivity.class);
             view.getContext().startActivity(intent);});
+    }
+
+    private class GetAuthorizationTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                InputStream responseStream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(responseStream));
+
+                StringBuilder result = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+                return result.toString();
+            } catch (IOException e) {
+                return "Error: " + e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONArray jsonArray = new JSONArray(result);
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    int client_id = jsonObject.getInt("id");
+                    String email = jsonObject.getString("email");
+                    String password = jsonObject.getString("password");
+
+                    AuthorizationInfo authorizationInfo = new AuthorizationInfo(client_id, email, password);
+
+                    authorizationInformation.add(authorizationInfo);
+                }
+            } catch (JSONException e) {}
+        }
     }
 }
